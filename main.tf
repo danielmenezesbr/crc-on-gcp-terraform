54ffccffab614af4d74f6a7e19d2e91c853eab15
danielmenezesbr <danielmenezes@gmail.com>
2021-08-06 22:44:06 -0300

    snc 4.6

--- main.tf
+++ main.tf
@@ -98,8 +98,8 @@ resource "google_compute_instance" "crc-build-box" {
 
   boot_disk {
     initialize_params {
-      #image = google_compute_image.crcimg.self_link
-      image = "projects/okd4-280016/global/images/packer-1597358211"
+      image = google_compute_image.crcimg.self_link
+      #image = "projects/okd4-280016/global/images/packer-1597358211"
       type  = var.gcp_vm_disk_type
       size  = var.gcp_vm_disk_size
     }
