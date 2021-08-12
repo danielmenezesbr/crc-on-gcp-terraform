f15f8d6496cf1895f603fd534a7573a27c1a7f83
danielmenezesbr <danielmenezes@gmail.com>
2021-08-12 20:40:59 -0300

    mnc with ocp-libvirt-ipi-role

--- servicemesh-install-OCPv46.sh
+++ servicemesh-install-OCPv46.sh
@@ -1,5 +1,5 @@
 set -exuo pipefail
-if [ "$(whoami)" != "crcuser" ] && [ "$(whoami)" != "crcuser" ]; then
+if [ "$(whoami)" != "crcuser" ] && [ "$(whoami)" != "root" ]; then
     echo "Error. Please log-in with crcuser (CRC/SNC) / root (MNC)"
     exit 1
 fi
